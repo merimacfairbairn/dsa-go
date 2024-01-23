acdf63ff4c82f4dff0c4d750a0c61c22a008e181
Larry <eirikrinnrauthr@tutanota.com>
2024-01-23 23:15:56 +0000

    Create a test for maze-solver

---
+++ maze/maze_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMaze(t *testing.T) {
+	maze := []string{
+		"xxxxxxxxxx x",
+		"x        x x",
+		"x        x x",
+		"x xxxxxxxx x",
+		"x          x",
+		"x xxxxxxxxxx",
+	}
+	start := Point{10, 0}
+	end := Point{1, 5}
+	want := []Point{
+		{x: 10, y: 0},
+		{x: 10, y: 1},
+		{x: 10, y: 2},
+		{x: 10, y: 3},
+		{x: 10, y: 4},
+		{x: 9, y: 4},
+		{x: 8, y: 4},
+		{x: 7, y: 4},
+		{x: 6, y: 4},
+		{x: 5, y: 4},
+		{x: 4, y: 4},
+		{x: 3, y: 4},
+		{x: 2, y: 4},
+		{x: 1, y: 4},
+		{x: 1, y: 5},
+	}
+	got := mazeSolver(maze, 'x', start, end)
+	for i := range got {
+		if got[i] != want[i] {
+			t.Errorf("Expected %v, got %v", want, got[i])
+		}
+	}
+}
