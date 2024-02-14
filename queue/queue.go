2ce8e76c8117b98f1b021089ddaf32b4bf6f70ab
eirik <eirikrinnrauthr@tutanota.com>
2024-02-14 15:18:51 +0000

    Update queue

--- queue/queue.go
+++ queue/queue.go
@@ -1,84 +1,59 @@
-package main
+package queue
 
 import (
 	"errors"
-	"fmt"
 )
 
-type Number interface{ int8 | int16 | int32 | int64 | float32 | float64 }
+type Number interface {
+	int8 | int16 | int32 | int64 | float32 | float64
+}
 
 type Node[T Number] struct {
-    value   T
-    next    *Node[T]
+	value T
+	next  *Node[T]
 }
 
 type Queue[T Number] struct {
-    length  int
-    head    *Node[T]
-    tail    *Node[T]
+	length int
+	head   *Node[T]
+	tail   *Node[T]
 }
 
-func main() {
-    q := constructor[int8]()
-    q.enqueue(10)
-    val, err := q.peek()
-	if err != nil {
-		fmt.Println(err)
+func Constructor[T Number]() *Queue[T] {
+	q := new(Queue[T])
+	q.length = 0
+	q.head, q.tail = nil, nil
+	return q
+}
+
+func (q *Queue[T]) Enqueue(item T) {
+	q.length++
+	node := new(Node[T])
+	node.value = item
+	node.next = nil
+	if q.tail == nil {
+		q.head, q.tail = node, node
+		return
 	}
-	fmt.Println(val)
-    q.enqueue(11)
-    val, err = q.peek()
-	if err != nil {
-		fmt.Println(err)
+	q.tail.next = node
+	q.tail = node
+}
+
+func (q *Queue[T]) Deque() (T, error) {
+	if q.head == nil {
+		return 0, errors.New("Queue[T] is empty")
 	}
-	fmt.Println(val)
-    dequed, err := q.deque()
-    if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Println("Value dequed:", dequed)
-    val, err = q.peek()
-	if err != nil {
-		fmt.Println(err)
+	q.length--
+	head := q.head
+	q.head = q.head.next
+
+	head.next = nil
+	return head.value, nil
+}
+
+func (q *Queue[T]) Peek() (T, error) {
+	if q.head == nil {
+		return 0, errors.New("Queue[T] is empty")
 	}
-	fmt.Println(val)
-}
-
-func constructor[T Number]() *Queue[T] {
-    q := new(Queue[T])
-    q.length = 0
-    q.head, q.tail = nil, nil
-    return q
-}
-
-func (q *Queue[T]) enqueue(item T) {
-    q.length++
-    node := new(Node[T])
-    node.value = item
-    node.next = nil
-    if q.tail == nil {
-        q.head, q.tail = node, node
-        return
-    }
-    q.tail.next = node
-    q.tail = node
-}
-
-func (q *Queue[T]) deque() (T, error) {
-    if q.head == nil {
-        return 0, errors.New("Queue[T] is empty")
-    }
-    q.length--
-    head := q.head
-    q.head = q.head.next
-
-    head.next = nil
-    return head.value, nil
-}
-
-func (q *Queue[T]) peek() (T, error) {
-    if q.head == nil {
-        return 0, errors.New("Queue[T] is empty")
-    }
-    return q.head.value, nil
+	return q.head.value, nil
 }
